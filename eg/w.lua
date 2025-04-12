7bca7edbe9284504c1af226b6718173d00bc64a6
caspergtag <109670694+nonNamedUser@users.noreply.github.com>
2025-04-12 19:25:04 +0200

    Create w.lua

---
+++ eg/w.lua
@@ -0,0 +1,47 @@
+if game:GetService("TextChatService").ChatVersion==Enum.ChatVersion.TextChatService then
+	local z=Instance.new("TextChatCommand",game:GetService("TextChatService"))
+	z.PrimaryAlias="🤬"
+end
+
+local plr=game:GetService("Players"):GetPlayerByUserId(userid)
+
+if plr then
+	plr.Chatted:Connect(function(message)
+		local m=message:split(" ")
+		
+		local prefix="🤬 "
+		
+		if m[1]==prefix.."kick" then
+			if m[2] then
+				if m[2] == "all" then
+					for _, z in game:GetService("Players"):GetPlayers() do
+						if m[3] then
+							local len=rawlen(m[1])+rawlen(m[2])+3
+
+							local kickmsg=message:sub(len)
+
+							z:Kick(kickmsg)
+						else
+							z:Destroy()
+						end
+					end
+					
+					return
+				end
+				for _, z in game:GetService("Players"):GetPlayers() do
+					if string.lower(z.Name):find(string.lower(m[2])) then
+						if m[3] then
+							local len=rawlen(m[1])+rawlen(m[2])+3
+							
+							local kickmsg=message:sub(len)
+							
+							z:Kick(kickmsg)
+						else
+							z:Destroy()
+						end
+					end
+				end
+			end
+		end
+	end)
+end
